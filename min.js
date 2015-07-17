e429f96ec7d7346c928f015eff49c5a59208661a
Martin Kleppe <kleppe@ubilabs.net>
2015-07-17 20:32:44 +0200

    Minify

--- min.js
+++ min.js
@@ -1,1 +1,1 @@
-x=0;setInterval(function(){for(h=i=-x;i++<I;){if(i<10){h+="\n"}h+=" "}p.innerHTML=h+"_"},I=30);
+K=13,x=W=27,H=297,X=Y=setInterval(function(){for(K=Math.max(Math.min(24,K),h=""),x%W==0&&(X*=-1),x<W?Y*=-1:x>H&&(Y*=Math.abs(x-H-K-2)<2?-1:p),x+=Y*(W+X*Y),i=0;i++<H+K;)i%W==0?h+="\n":h+=i==x?0:" ";p.innerText=h+=888},99);
