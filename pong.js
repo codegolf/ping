cbaecdd4353d0f692011cf0fcc4c98893f72c24c
Martin Kleppe <kleppe@ubilabs.net>
2015-07-17 20:40:10 +0200

    213

--- pong.js
+++ pong.js
@@ -7,15 +7,17 @@ X = Y =
 setInterval(function() {
     for (
       K = Math.max(Math.min(24, K), h = ""),
-      x % W == 0 && (X *= -1),
+      x % W || (X *= -1),
       x < W ?
         Y *= -1 :
         x > H && (
           Y *= Math.abs(x - H - K - 2) < 2 ? -1 : p
         ),
-      x += Y * (W + X * Y), i = 0; i++ < H + K;
-    ) i % W == 0 ?
-      h += "\n" :
-      (h += i == x ? 0 : " ");
+      x += Y * (W + X * Y),
+      i = 0;
+      i++ < H + K;
+    )
+      h += i % W ?
+        i == x ? 0 : " " : "\n";
     p.innerText = h += 888
 }, 99);
