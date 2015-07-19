0f0e9067c73dc0548059065859f1e31f281b1fbf
Martin Kleppe <kleppe@ubilabs.net>
2015-07-19 16:26:16 +0200

    reformat

--- min.js
+++ min.js
@@ -1,1 +1,1 @@
-K=13,x=W=27,X=Y=setInterval(function(){for(i=h="",K=K<0?0:K>24?24:K,X*=x%W?1:-1,Y*=x<W?-1:x>297?(Q=x-299-K)*Q<4?-1:p:1,x+=Y*(W+X*Y);i++<297+K;p.innerText=h+888)h+=i%W?i==x?0:" ":"\n"},99);
+K=13,x=W=27,X=Y=setInterval(function(){for(i=h="",K=K<0?0:24<K?24:K,X*=x%W?1:-1,Y*=x<W?-1:297<x?(Q=x-299-K)*Q<4?-1:p:1,x+=Y*(W+X*Y);i++<297+K;p.innerText=h+888)h+=i%W?i^x?" ":0:"\n"},99);
