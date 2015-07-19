c0d7c39a85a27daf6a0e7b771041cc22b644bcb3
Martin Kleppe <kleppe@ubilabs.net>
2015-07-19 23:06:40 +0200

    Fix mixed tabs and spaces.

--- annotated.js
+++ annotated.js
@@ -11,18 +11,18 @@ setInterval(function() {
     h = "",                     // output string
 
     k = k < 0 ? 0 :             // keep paddle in bounds
-    	24 < k ? 24 :
-    	k,
+      24 < k ? 24 :
+      k,
 
     X = x % W ?                // reflect if ball hits wall
-    	X :
-    	-X,
+      X :
+      -X,
 
     Y =                         // change Y direction
       x < 0 ? 1 :               // reflect if ball hit top border
         297 < x ? (             // if ball hits bottom border ...
           (
-          	Q = x - 299 - k     // calculate distance to paddle center
+            Q = x - 299 - k     // calculate distance to paddle center
           ) * Q < 4 ?           // if distance^2 is less than four
             -1 :                // ... reflect up
             p                   // otherwise break the game
